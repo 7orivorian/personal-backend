2bf2d42011d077420a75163b7c13f8d9bb0e56b8
Tori <7orivorian+github@gmail.com>
2025-01-13 21:57:41 -0500

    Refactor database URI construction for better configurability.
    Replaced single environment variable with individual components for database connection string. This improves flexibility and aligns with best practices for environment-based configurations. Existing functionality remains unchanged.

--- app/config.py
+++ app/config.py
@@ -3,7 +3,14 @@ import os
 
 class Config:
     FLASK_ENV = os.getenv("FLASK_ENV")
-    SQLALCHEMY_DATABASE_URI = os.getenv("MARIADB_PRIVATE_URL")
+
+    username = os.getenv("MARIADB_USER")
+    password = os.getenv("MARIADB_PASSWORD")
+    host = os.getenv("MARIADB_HOST")
+    port = os.getenv("MARIADB_PORT")
+    database = os.getenv("MARIADB_DATABASE")
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
 
