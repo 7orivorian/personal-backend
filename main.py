bd4612cdd3c9e7d8089ad1615cf27da66dbb94b5
Tori <7orivorian+github@gmail.com>
2025-01-27 02:31:52 -0500

    Add CORS headers to responses
    Implemented a global `after_request` function to include CORS headers. This ensures proper handling of cross-origin requests from the specified domain. Adjusted the app initialization to align with the new middleware structure.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 
 from app import create_app, db
 
+app = create_app()
+
 
 def clean_revoked_tokens():
     expiration = datetime.now() - timedelta(days=30)  # Example: 30-day expiration
@@ -11,7 +13,13 @@ def clean_revoked_tokens():
     db.session.commit()
 
 
-app = create_app()
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'https://princeling.dev')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 
 if __name__ == "__main__":
     with app.app_context():
